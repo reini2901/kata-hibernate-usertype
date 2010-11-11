26af249c92b7cfa8e9e02edd270660ec128ba9af
rruiner <reinhard.ruiner@gmail.com>
2010-11-11 18:21:43 +0100

    better logging

--- src/test/java/hibernate/UserTypeTest.java
+++ src/test/java/hibernate/UserTypeTest.java
@@ -4,6 +4,7 @@ import hibernate.util.HibernateUtil;
 
 import java.util.List;
 
+import static org.testng.Assert.*;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -19,17 +20,25 @@ public class UserTypeTest  {
 
 		mgr.storeSimple(NAME);
 
+		assertEquals(checkDb(mgr),1);
+
+	}
+
+	private int checkDb(Manager mgr) {
 		List<SimpleEntity> all = mgr.findAllSimple();
 		for (SimpleEntity simpleEntity : all) {
 			Assert.assertEquals(simpleEntity.getUserName(), NAME);
 		}
-
+		System.out.println("stored in db: " + all.size());
+		return all.size();
 	}
 
 	@Test
 	public void test2() {
 		Manager mgr = new Manager();
 		mgr.storeEnhanced(NAME);
+		
+		assertEquals(checkDb(mgr),2);
 		
 		List<BetterEntity> all = mgr.findAllBetter();
 		for (BetterEntity e : all) {
